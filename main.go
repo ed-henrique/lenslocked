5b823e17b64d059145b06b52634402be6159e29a
Eduardo Henrique Freire Machado <edu.hen.fm@gmail.com>
2025-04-24 12:34:54 -0400

    feat: add simple error handling

--- main.go
+++ main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
 	"net/http"
 )
 
 func HandlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Welcome to my awesome site!</h1>")
+	_, err := fmt.Fprintf(w, "<h1>Welcome to my awesome site!</h1>")
+	if err != nil {
+		slog.Error("the message could not be sent", slog.String("err", err.Error()))
+	}
 }
 
 func main() {
 	http.HandleFunc("/", HandlerFunc)
 	fmt.Println("Starting the server on :8080...")
-	http.ListenAndServe(":8080", nil)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		slog.Error("could not start the server", slog.String("err", err.Error()))
+	}
 }
